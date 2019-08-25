164ce16f167a2d8358f287bc7991810ef8520357
Anitha987 <anithaumuhire@gmail.com>
2019-08-25 08:31:29 -0400

    add paragraph

--- Anny/Quotes/src/app/quote/quote.component.ts
+++ Anny/Quotes/src/app/quote/quote.component.ts
@@ -23,7 +23,7 @@ export class QuoteComponent implements OnInit {
       }
     }
   }
-  addNewQuote(quote){
+  addNewGoal(quote){
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
     quote.completeDate = new Date(quote.completeDate)
