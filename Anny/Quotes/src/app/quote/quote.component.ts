2c2fe8abefe09bcbe8c224e9a6052e659d44666d
Anitha987 <anithaumuhire@gmail.com>
2019-08-25 08:08:24 -0400

    add constructors

--- Anny/Quotes/src/app/quote/quote.component.ts
+++ Anny/Quotes/src/app/quote/quote.component.ts
@@ -7,18 +7,27 @@ import {Quote}from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote[]=[
-    new Quote(1,'Turn your wounds into wisdom','anitha'),
-    new Quote(2,'Tough times never last, but tough people do','flower'),
-    new Quote(3,'The day is what you make it! So why not make it a great one?','blandine'),
-    new Quote(4,'It is never too late to be what you might have been.','laetitia'),
+    new Quote(1,'Turn your wounds into wisdom','William Shakespeare ',new Date(2018,3,19)),
+    new Quote(2,'Tough times never last, but tough people do',' Mark Twain',new Date(2018,1,9)),
+    new Quote(3,'The day is what you make it! So why not make it a great one?',' Ernest Hemingway',new Date(2018,6,29)),
+    new Quote(4,'It is never too late to be what you might have been.','Stephen King',new Date(2018,5,1)),
   ];
   toggleDetails(index){
     this.quotes[index].showAuthor = !this.quotes[index].showAuthor;
   }
-  completeGoal(isComplete, index){
+  deleteGoal(isComplete, index){
     if (isComplete) {
+      let toDelete=confirm('are you sure you want to delete ${this.quotes[index].name}?')
+      if(toDelete){
       this.quotes.splice(index,1);
+      }
     }
+  }
+  addNewQuote(quote){
+    let quoteLength = this.quotes.length;
+    quote.id = quoteLength+1;
+    quote.completeDate = new Date(quote.completeDate)
+    this.quotes.push(quote)
   }
   constructor() { }
 
