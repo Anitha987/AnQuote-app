1126a2e5bf2e33a31d66f75bf819e6940438bfb5
Anitha987 <anithaumuhire@gmail.com>
2019-08-25 08:44:26 -0400

    add head

--- Anny/Quotes/src/app/quote-form/quote-form.component.ts
+++ Anny/Quotes/src/app/quote-form/quote-form.component.ts
@@ -7,13 +7,5 @@ import { Quote} from '../quote';
 })
 export class QuoteFormComponent implements OnInit {
   newQuote = new Quote(0,"","",new Date());
-@Output() addQuote = new EventEmitter<Quote>();
-submitGoal(){
-  this.addQuote.emit(this.newQuote);
-    }
-  constructor() { }
-
-  ngOnInit() {
-  }
 
 }
