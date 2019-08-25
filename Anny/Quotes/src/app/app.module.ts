63a00baec3f74274558c37ffb6fe5d7ca729d222
Anitha987 <anithaumuhire@gmail.com>
2019-08-25 08:43:00 -0400

    add paragraph

--- Anny/Quotes/src/app/app.module.ts
+++ Anny/Quotes/src/app/app.module.ts
@@ -1,7 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { QuoteComponent } from './quote/quote.component';
 import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
@@ -18,11 +17,7 @@ import { QuoteFormComponent } from './quote-form/quote-form.component';
     DateCountPipe,
     QuoteFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
