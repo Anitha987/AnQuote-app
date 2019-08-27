134ca30fb77292f32c0b53741c008629b92618ad
Anitha987 <anithaumuhire@gmail.com>
2019-08-27 04:45:56 -0400

    styling

--- Anny/Quotes/src/app/app.component.ts
+++ Anny/Quotes/src/app/app.component.ts
@@ -6,10 +6,10 @@ import{Quote}from './quote';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // quotes:Quote[]=[
+  quotes:Quote[]=[
   //   new Quote(1,'Turn your wounds into wisdom','anitha'),
   //   new Quote(2,'Tough times never last, but tough people do','flower'),
   //   new Quote(3,'The day is what you make it! So why not make it a great one?','blandine'),
   //   new Quote(4,'It is never too late to be what you might have been.','laetitia'),
-  // ];
+  ];
 }
